a14f420ea214fa5ea874f122ba5db95f08cf73f7
cuonglm210498 <lemanhcuong0498@gmail.com>
2022-06-26 10:53:27 +0700

    create package

---
+++ src/main/java/com/lecuong/java09springboot/Java09SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lecuong.java09springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java09SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java09SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lecuong/java09springboot/Java09SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecuong.java09springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java09SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
