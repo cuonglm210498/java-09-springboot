4380cfdf78808f517704c426910c26988b3ab79c
cuonglm18 <cuonglm18@viettelsoftware.vn>
2022-08-10 08:04:24 +0700

    add package common and class DateTimeCommon and using in UserResponse

--- src/main/java/com/lecuong/java09springboot/modal/response/user/UserResponse.java
+++ src/main/java/com/lecuong/java09springboot/modal/response/user/UserResponse.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.lecuong.java09springboot.common.DateTimeCommon;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -23,9 +24,10 @@ public class UserResponse {
     private String fullName;
     private String address;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @JsonDeserialize(using = LocalDateDeserializer.class)
-    @JsonSerialize(using = LocalDateSerializer.class)
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+//    @JsonDeserialize(using = LocalDateDeserializer.class)
+//    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(pattern = DateTimeCommon.DATE_TIME_FORMAT.DD_MM_YYYY, timezone = DateTimeCommon.DATE_TIME_FORMAT.TIME_ZONE)
     private LocalDate dob;
 
     private String facebook;
